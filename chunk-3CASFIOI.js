35e273315bb4825df5cdde929e98ea96f504f4ae
AmasMovsisian <232450988+AmasMovsisian@users.noreply.github.com>
2026-09-13 14:22:41 +0200

    Auto-generated commit

---
+++ chunk-3CASFIOI.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-DTV3WO3J.js";import"./chunk-VMXTGYAL.js";import"./chunk-LVWHMYNX.js";export{a as Contact};
--- chunk-SRPYX5R7.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-CK2L7GO6.js";import"./chunk-6QAS3HUB.js";import"./chunk-EBP2MOFU.js";export{a as Contact};
